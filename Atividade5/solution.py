ec7a5af2c27c2a658a838ae8a08c28d2d4f805a5
Mateus Souza <mateushen19@outlook.com>
2025-01-08 15:20:47 -0300

    feat: adiciona comentarios

--- Atividade5/solution.py
+++ Atividade5/solution.py
@@ -1,9 +1,9 @@
 def calculate(n):
-    divisor = 1 
+    divisor = 1 # Divisor inicial
     length = 1
 
     while divisor % n != 0:
-        divisor = (divisor * 10 + 1) % n 
+        divisor = (divisor * 10 + 1) % n # Calculo para saber qual o proximo divisor 
         length += 1 
 
     return length
